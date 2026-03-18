e2f6bfcef9bdd67a2f1b7df94f9c4d5337d9d3eb
Shubham Singhal <shubhamsinghal@MacBook-Air-465.local>
2026-03-18 10:10:03 -0500

    Configure Vite proxy and build settings

---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../public',
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8787',
+        changeOrigin: true,
+      }
+    }
+  }
+})
